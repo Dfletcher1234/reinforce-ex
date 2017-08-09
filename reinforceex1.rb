02c8d8a0d1f350e65a1838a2b8bfe6a2b15d1618
David FletcherE <dtsfletcher@gmail.com>
2017-08-09 15:09:08 -0400

    number 1 done

--- reinforceex1.rb
+++ reinforceex1.rb
@@ -9,26 +9,25 @@ answer = gets.chomp
 if answer == "yes"
 puts "I recommend #{documentary}"
 
-puts "Do you enjoy dramas?"
-answer2 = gets.chomp
-puts "Do you enjoy comedies?"
-answer3 = gets.chomp
+else
 
-# if answer == "yes"
-#   puts "I recommend #{documentary}"
+  puts "Do you enjoy dramas?"
+  answer2 = gets.chomp
+  puts "Do you enjoy comedies?"
+  answer3 = gets.chomp
 
-elsif answer == "no" && answer2 == "yes" && answer3 == "yes"
-  puts "I recommedn #{dramedy}"
+  if  answer2 == "yes" && answer3 == "yes"
+    puts "I recommedn #{dramedy}"
 
-elsif answer == "no" && answer2 == "yes" && answer3 == "no"
-  puts "I recommend #{drama}"
+  elsif answer2 == "yes" && answer3 == "no"
+    puts "I recommend #{drama}"
 
-elsif answer == "no" && answer2 == "no" && answer3 == "yes"
-  puts "I recommend #{comedy}"
+  elsif  answer2 == "no" && answer3 == "yes"
+    puts "I recommend #{comedy}"
 
-elsif answer == "no" && answer2 == "no" && answer3 == "no"
-  puts "I suggest you read a book"
+  else  answer2 == "no" && answer3 == "no"
+    puts "I suggest you read a book"
 
-
+ end
 
 end
